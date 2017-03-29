d28c2d311ef4bd20857570db4a56a6aa22f7a5a8
jonamatthew <jonathan.abraham.matthew.info@gmail.com>
2017-03-29 09:03:19 -0500

    added literature

--- RScript/RBF.R
+++ RScript/RBF.R
@@ -1,11 +1,24 @@
 # RADIAL BASIS FUNCTION========================================================
 # LOAD-------------------------------------------------------------------------
 library(RSNNS)
+library(plotly)
 
 
 # IDEAS------------------------------------------------------------------------
+
 # Parks may be centers (input layer?), a function (gaussian?) may be the
 # hidden layer, and the crime locations may be the expected output layer
 # The weight from the input to the hidden layer may be given by some function
 # The weight from the hidden layer to the output may be given by some linear
 # function.
+
+
+
+# OUTPUT: density of crime
+# INPUT: distance from park
+# CENTER: park
+
+
+
+# Using plotly to 3D hist the data
+# Looking at the hist of distances
