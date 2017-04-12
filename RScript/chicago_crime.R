dbc5fa1b56540ab87a6a72aa8c0b079a5b6750db
jonamatthew <jonathan.abraham.matthew.info@gmail.com>
2017-04-12 15:58:40 -0500

    modified master script (chicago_crime.R) added violentxy.RData (mercator projection of locations of all violent crime data)

--- RScript/chicago_crime.R
+++ RScript/chicago_crime.R
@@ -1,154 +1,138 @@
-# MASTER SCRIPT FOR CHICAGO_CRIME PROJECT######################################
-	# MODIFY PARKS DATA============================================================
+# CHICAGO CRIME################################################################
 
-		# READ/TIDY PARKS SHAPEFILES---------------------------------------------------
-library(rgdal)
-library(broom)
-library(maptools)
-park <- readOGR(dsn="Shapefiles/Parks_Aug2012", layer="Parks_Aug2012")
-parkdat <- tidy(park, region="PARK")
-parkpoly <- cbind(parkdat$long, parkdat$lat)
-colnames(parkpoly) <- c("long", "lat")
+	# PARKS SHAPEFILES=============================================================
+	library(rgdal)
+	library(broom)
+	library(maptools)
+	park <- readOGR(dsn="./Shapefiles/Parks_Aug2012", layer="Parks_Aug2012")
+	save(park, file="RData/park.RData")
+	parkdat <- tidy(park, region="PARK")
+	parkdat$parknumber <- as.integer(factor(parkdat$id))
+	save(parkdat, file="RData/parkdat.RData")
 
-		# SAVE-------------------------------------------------------------------------
-save(park, file="RData/park.RData")
-save(parkdat, file="RData/parkdat.RData")
-save(parkpoly, file="RData/parkpoly.RData")
+	# CRIME DATA PROJECTION========================================================
+	load("RData/violentdat.RData")
+	load("RData/park.RData")
+	violentll <- na.omit(cbind(violentdat$Longitude, violentdat$Latitude))
+	colnames(violentll) <- c("longitude", "latitude")
+	llproj <- '+init=epsg:4269'
+	violentsp <- SpatialPoints(na.omit(violentll), proj4string=CRS(llproj))
+	library(sp)
+	violentmerc <- spTransform(violentsp, CRS(proj4string(park)))
+	violentxy <- coordinates(violentsp)
+	save(violentxy, file="RData/violentxy.RData")
 
-
-	# MODIFY ASSAULT DATA==========================================================
-
-		# READ IN ASSAULT DATA---------------------------------------------------------
-assaultdat <- read.csv("CSV/ChicagoAssaults.csv", stringsAsFactors=FALSE)
-save(assaultdat, file="RData/assaultdat.RData")
-
-		# MATRIX OF LATITUDES AND LONGITUDES OF ASSAULTS-------------------------------
-assaultlatlong <- na.omit(cbind(assaultdat$Longitude, assaultdat$Latitude))
-colnames(assaultlatlong) <- c("longitude", "latitude")
-head(assaultlatlong)
-class(assaultlatlong)
-
-		# CONVERT ASSAULTS TO SPATIAL POINTS FORMAT------------------------------------
-llproj <- '+init=epsg:4269'
-assaultsp <- SpatialPoints(na.omit(assaultlatlong), proj4string=CRS(llproj))
-
-		# CONVERT ASSAULTS TO MERCATOR PROJECTION--------------------------------------
-assaultmerc <- spTransform(assaultsp, CRS(proj4string(park)))
-
-		# CONVERT ASSAULTS TO COORDINATES----------------------------------------------
-assaultxy <- coordinates(assaultmerc)
-
-		# SAVE-------------------------------------------------------------------------
-save(assaultxy, file="RData/assaultxy.RData")
-
-
-	# DISSOLVE POLYGON=============================================================
-
-		# CREDIT-----------------------------------------------------------------------
-# modified from: https://philmikejones.wordpress.com/2015/09/03/dissolve-polygons-in-r/
-
-		# LOAD PACKAGES----------------------------------------------------------------
-library(rgdal)
-library(rgeos)
-library(dplyr)
+	# REGION POLYGON=============================================
+	# modified from: https://philmikejones.wordpress.com/2015/09/03/dissolve-polygons-in-r/
 
 		# READ AND MODIFY SHAPEFILES---------------------------------------------------
-region <- readOGR(dsn="Shapefiles/CommAreas", layer="CommAreas")
-plot(region)
-lookup <- data.frame()
-lookup <- rbind(lookup, region@data)
-lookup$AREA_NUMBE <- as.character(lookup$AREA_NUMBE)
-lookup$COMMUNITY <- as.character(lookup$COMMUNITY)
-lookup$CITY <- NA
-lookup$CITY <- "Chicago"
+		library(rgdal)
+		region <- readOGR(dsn="Shapefiles/CommAreas", layer="CommAreas")
+		plot(region)
+		lookup <- data.frame()
+		lookup <- rbind(lookup, region@data)
+		lookup$AREA_NUMBE <- as.character(lookup$AREA_NUMBE)
+		lookup$COMMUNITY <- as.character(lookup$COMMUNITY)
+		lookup$CITY <- NA
+		lookup$CITY <- "Chicago"
 
 		# MERGE------------------------------------------------------------------------
-region@data$AREA_NUMBE <- as.character(region@data$AREA_NUMBE)
-region@data <- full_join(region@data, lookup, by="AREA_NUMBE")
-row.names(region) <- row.names(region@data)
-region <- spChFIDs(region, row.names(region))
+		region@data$AREA_NUMBE <- as.character(region@data$AREA_NUMBE)
+		library(dplyr)
+		region@data <- full_join(region@data, lookup, by="AREA_NUMBE")
+		row.names(region) <- row.names(region@data)
+		region <- spChFIDs(region, row.names(region))
 
 		# DISSOLVE---------------------------------------------------------------------
-region <- gUnaryUnion(region, id=region@data$country)
+		library(rgeos)
+		region <- gUnaryUnion(region, id=region@data$country)
 
 		# MODIFY DATAFRAME-------------------------------------------------------------
-row.names(region) <- as.character(1:length(region))
-lookup <- unique(lookup$CITY)
-lookup <- as.data.frame(lookup)
-colnames(lookup) <- "CITY"
-region <- SpatialPolygonsDataFrame(region, lookup)
+		row.names(region) <- as.character(1:length(region))
+		lookup <- unique(lookup$CITY)
+		lookup <- as.data.frame(lookup)
+		colnames(lookup) <- "CITY"
+		region <- SpatialPolygonsDataFrame(region, lookup)
+		save(region, file="RData/region.RData")
 
-		# SAVE-------------------------------------------------------------------------
-save(region, file="RData/region.RData")
-	# POINT PROCESS MODELLING======================================================
+	# ANALYSIS=====================================================================
 
-# LOAD FILES
 		# LOAD-------------------------------------------------------------------------
-load("RData/assaultxy.RData")
-load("RData/region.RData")
-load("RData/park.RData")
+		load("RData/assaultxy.RData")
+		load("RData/region.RData")
+		load("RData/park.RData")
+		load("RData/parkdat.RData")
 
-		# KERNEL SMOOTHING-------------------------------------------------------------
-poly <- region@polygons[[1]]@Polygons[[3]]@coords # isolates the coordinates from the polygon
-library(splancs)
-# eventually do a sensitivity analysis to choose a bandwidth
-smooth <- kernel2d(assaultxy, poly, 1000, 200, 200)
-library(fields)
-image.plot(smooth); plot(park, add=TRUE)
+		# KERNEL SMOOTHING------------------------------------------------------------
+		poly <- region@polygons[[1]]@Polygons[[3]]@coords # isolates coordinates
+		library(splancs)
+		smooth <- kernel2d(assaultxy, poly, 5280, 500, 500) # hi res smoothing
+		# perform with 1/8 1/4 1/2 1 mile (660, 1320, 2640, 5280 feet) bandwidths
+		library(fields)
+		image.plot(smooth); plot(region, add=TRUE)
+		# image(smooth); plot(region, add=TRUE)
+		# image.plot(smooth); plot(park, add=TRUE)
 
-		# POINT PROCESS (NOT USED)-----------------------------------------------------
-#library(spatstat)
-#glass <- data.frame(x=rev(poly[,1]), y=rev(poly[,2]))
-#windowpane <- owin(poly=glass)
-#assaultpp <- as.ppp(assaultxy, windowpane)
+		# UPPER THRESHOLD ------------------------------------------------------------------
 
-		# CONGLOMERATE Z VALUES (NOT USED)---------------------------------------------
-#zval <- vector()
-#for (i in 1:100000) {
-#	xcoord <- which(smoothfine$x > assaultxy[i,1])[1]
-#	ycoord <- which(smoothfine$y > assaultxy[i,2])[1]
-#	zval[i] <- smoothfine$z[xcoord,ycoord]
-#}
+		# isolate matrix of crime density at each pixel
+		zval <- smooth$z
 
-# CALCULATE RETENTION PROBABILITY
-		# RETENTION PROBABILITY--------------------------------------------------------
-totalassaults <- nrow(assaultxy) # total number of assaults
-library(rgeos)
-chicagoarea <- gArea(region, byid=FALSE) # area of Chicago in sq ft
-pixelarea <- (smooth$x[2] - smooth$x[1]) * (smooth$y[2] - smooth$y[1]) # area of one pixel
-chicagopixels <- chicagoarea/pixelarea # number of pixels that make up Chicago
-lambda = smooth$z*totalassaults/chicagopixels # estimated true intensity from density for each pixel
-rho = matrix(nrow=nrow(lambda), ncol=ncol(lambda)) # desired intensity for near-CSR after thinning
-for(i in 1:200) {
-	for (j in 1:200) {
-		rho[i,j] <- 10
-	}
-}
+		# set upper threshold for isolating clusters
+		# e.g. 0.95 isolates the pixels that have the top 5% of crime densities
+		threshold <- quantile(zval[zval>0], .95, na.rm=TRUE)
 
-		# ASSOCIATE EACH CRIME WITH ITS LAMBDA-----------------------------------------
-lambdacol <- matrix(ncol=1, nrow=nrow(assaultxy)) # temporary column to hold lambda values
-assaultlambda <- cbind(assaultxy, lambdacol) # cbind assaultxy and lambdacol
-colnames(assaultlambda)[3] <- "lambda" # rename lambdacol
-for (i in 1:nrow(assaultlambda)) {
-	xcoord <- which(smooth$x > assaultlambda[i,1])[1] # temp holds first xcoord in smooth$x that's > assault's xcoord
-	ycoord <- which(smooth$y > assaultlambda[i,2])[1] # temp holds first ycoord in smooth$y that's > assault's ycoord
-	assaultlambda[i,3] <- lambda[xcoord, ycoord] # insert associated lambda value into matrix
-}
-save(assaultlambda, file="RData/assaultlambda.RData")
+		# make matrix indicating pixels where crime density exceeds threshold
+		crimepixels <- zval > threshold
 
-		# THINNING OF POINT PROCESS (IN PROGRESS)--------------------------------------
-library(spatstat)
-thin <- rthin(assaultpp, retprob)
-plot(thin)
-p <- (1/smoothfine$z)
-p <- ifelse(p==Inf, 1, p)
-smoothfine2 <- smoothfine
-smoothfine2$z <- p
-image.plot(smoothfine2)
+		# GROUP ----------------------------------------------------------------------
+		# source: http://stackoverflow.com/questions/35772846/obtaining-connected-components-in-r
 
+		# pixels that are nearby and exceed the threshold are clumped together
+		# each clump represents a hotspot
+		# directions=8 includes diagonals, directions=4 does not
+		# gaps=FALSE numbers and counts clumps without skips
+		library(raster)
+		crimeraster <- raster(crimepixels)
+		plot(crimeraster)
+		hotspots <- as.matrix(clump(crimeraster, directions=8, gaps=FALSE))
 
+		# make list that returns indices of each pixel in each hotspot
+		numhotspots <- max(hotspots, na.rm=TRUE)
+		hotspotlist <- vector("list", numhotspots)
+		for (i in 1:numhotspots){
+			hotspotlist[i] <- list(which(hotspots == i, arr.ind = TRUE))
+		}
 
+		# FIND DENSITY MODE OF HOTSPOT-------------------------------------------------
+		centers <- matrix(NA, length(hotspotlist), 2)
+		for(i in 1:length(hotspotlist)) {
+			zval <- vector()
+			for(j in 1:nrow(hotspotlist[[i]])){
+				zval[j] <- smooth$z[hotspotlist[[i]][j,1], hotspotlist[[i]][j,2]]
+			}
+			thismode <- which.max(zval)
+			centers[i,1] <- smooth$x[hotspotlist[[i]][thismode,1]]
+			centers[i,2] <- smooth$y[hotspotlist[[i]][thismode,2]]
+		}
+		centers <- data.frame(centers)
+		names(centers) <- c("x", "y")
 
+		# DISTANCE FROM PARK TO HOTSPOTS-----------------------------------------------
+		source("RScript/DIST.R")
+		source("RScript/DISTppoly.R")
+		# each park is a row and each hotspot is a column
+		# distancematrix <- matrix(data=NA, nrow=length(unique(parkdat$number)), ncol=nrow(centers))
+		# loop through every park calculating distances to every hotspot
+		# DISTppoly should place a vector of distances in each row
+		distances <- matrix(data=NA, nrow=length(unique(parkdat$parknumber)), ncol=nrow(centers))
+		for (i in 1:nrow(distances)) {
+			distances[i,] <- DISTppoly(pts=as.matrix(centers), poly=as.matrix(parkdat[parkdat$parknumber==i, 1:2]),
+						   method="Euclidean")
+		}
 
-
-
+		# MINIMUM DISTANCE TO HOTSPOT--------------------------------------------------
+		mymins <- data.frame(parknumber=1:nrow(distances))
+		mymins$mins <- apply(distances, 1, min)
+		mymins <- mymins[order(mymins[,2], decreasing=FALSE),] # order parks from closest minimum distance to hotspot to furthest
+		parkdat <- merge(parkdat, mymins, by="parknumber")
