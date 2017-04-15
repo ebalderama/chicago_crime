f4bbc3d1cc8b9ded7175f2e25831ad4c3636cd37
jonamatthew <jonathan.abraham.matthew.info@gmail.com>
2017-04-14 22:27:26 -0500

    added graphics, updated master script

--- RScript/chicago_crime.R
+++ RScript/chicago_crime.R
@@ -19,7 +19,7 @@
 	violentsp <- SpatialPoints(na.omit(violentll), proj4string=CRS(llproj))
 	library(sp)
 	violentmerc <- spTransform(violentsp, CRS(proj4string(park)))
-	violentxy <- coordinates(violentsp)
+	violentxy <- coordinates(violentmerc)
 	save(violentxy, file="RData/violentxy.RData")
 
 	# REGION POLYGON=============================================
@@ -58,45 +58,126 @@
 	# ANALYSIS=====================================================================
 
 		# LOAD-------------------------------------------------------------------------
-		load("RData/assaultxy.RData")
+		load("RData/violentxy.RData")
 		load("RData/region.RData")
-		load("RData/park.RData")
 		load("RData/parkdat.RData")
 
 		# KERNEL SMOOTHING------------------------------------------------------------
 		poly <- region@polygons[[1]]@Polygons[[3]]@coords # isolates coordinates
 		library(splancs)
-		smooth <- kernel2d(assaultxy, poly, 5280, 500, 500) # hi res smoothing
-		# perform with 1/8 1/4 1/2 1 mile (660, 1320, 2640, 5280 feet) bandwidths
 		library(fields)
-		image.plot(smooth); plot(region, add=TRUE)
-		# image(smooth); plot(region, add=TRUE)
-		# image.plot(smooth); plot(park, add=TRUE)
+		# perform with 1/8 1/4 1/2 1 mile (660, 1320, 2640, 5280 feet) bandwidths
+		
+		# EIGHTH MILE
+		eighthsmooth <- kernel2d(violentxy, poly, 660, 2000, 2000)
+		png(file="Graphics/eighthsmooth.png", width=2000, height=2000)
+		image(eighthsmooth, col=tim.colors(), axes=FALSE, bty="n")
+		plot(region, add=TRUE, lwd=10)
+		box(which="plot", lty="solid", lwd=10)
+		dev.off()
+		
+		# QUARTER MILE
+		quartersmooth <- kernel2d(violentxy, poly, 1320, 2000, 2000)
+		png(file="Graphics/quartersmooth.png", width=2000, height=2000)
+		image(quartersmooth, col=tim.colors(), axes=FALSE, bty="n")
+		plot(region, add=TRUE, lwd=10)
+		box(which="plot", lty="solid", lwd=10)
+		dev.off()
+		
+		# HALF MILE
+		halfsmooth <- kernel2d(violentxy, poly, 2640, 2000, 2000)
+		png(file="Graphics/halfsmooth.png", width=2000, height=2000)
+		image(halfsmooth, col=tim.colors(), axes=FALSE, bty="n")
+		plot(region, add=TRUE, lwd=10)
+		box(which="plot", lty="solid", lwd=10)
+		dev.off()
+		
+		# WHOLE MILE
+		wholesmooth <- kernel2d(violentxy, poly, 5280, 2000, 2000)
+		png(file="Graphics/wholesmooth.png", width=2000, height=2000)
+		image(wholesmooth, col=tim.colors(), axes=FALSE, bty="n")
+		plot(region, add=TRUE, lwd=10)
+		box(which="plot", lty="solid", lwd=10)
+		dev.off()
 
 		# UPPER THRESHOLD ------------------------------------------------------------------
-
 		# isolate matrix of crime density at each pixel
-		zval <- smooth$z
-
+		# zval <- smooth$z
 		# set upper threshold for isolating clusters
 		# e.g. 0.95 isolates the pixels that have the top 5% of crime densities
-		threshold <- quantile(zval[zval>0], .95, na.rm=TRUE)
-
+		# threshold <- quantile(zval[zval>0], .95, na.rm=TRUE)
 		# make matrix indicating pixels where crime density exceeds threshold
-		crimepixels <- zval > threshold
+		# crimepixels <- zval > threshold
+		
+		# EIGHTH MILE
+		eighthzval <- eighthsmooth$z
+		eighththreshold <- quantile(eighthzval[eighthzval>0], .95, na.rm=TRUE)
+		eighthpixels <- eighthzval > eighththreshold
+		
+		# QUARTER MILE
+		quarterzval <- quartersmooth$z
+		quarterthreshold <- quantile(quarterzval[quarterzval>0], .95, na.rm=TRUE)
+		quarterpixels <- quarterzval > quarterthreshold
+		
+		# HALF MILE
+		halfzval <- halfsmooth$z
+		halfthreshold <- quantile(halfzval[halfzval>0], .95, na.rm=TRUE)
+		halfpixels <- halfzval > halfthreshold
+		
+		# WHOLE MILE
+		wholezval <- wholesmooth$z
+		wholethreshold <- quantile(wholezval[wholezval>0], .95, na.rm=TRUE)
+		wholepixels <- wholezval > wholethreshold
 
 		# GROUP ----------------------------------------------------------------------
 		# source: http://stackoverflow.com/questions/35772846/obtaining-connected-components-in-r
-
 		# pixels that are nearby and exceed the threshold are clumped together
 		# each clump represents a hotspot
 		# directions=8 includes diagonals, directions=4 does not
 		# gaps=FALSE numbers and counts clumps without skips
 		library(raster)
-		crimeraster <- raster(crimepixels)
-		plot(crimeraster)
+		# crimeraster <- raster(crimepixels)
+		# image(t(flip(crimeraster,1)), col=c("gray", "red"),
+		#       axes=FALSE, xlab="", ylab="")
+		# box(which = "plot", lty = "solid")
+		
+		# EIGHTH MILE
+		eighthraster <- raster(eighthpixels)
+		png(file="Graphics/eighthraster.png", width=2000, height=2000)
+		image(t(flip(eighthraster,1)), col=c("gray20", "firebrick1"),
+		      axes=FALSE, xlab="", ylab="", bty="n")
+		box(which = "plot", lty = "solid", lwd=10)
+		dev.off()
+		
+		# QUARTER MILE
+		quarterraster <- raster(quarterpixels)
+		png(file="Graphics/quarterraster.png", width=2000, height=2000)
+		image(t(flip(quarterraster,1)), col=c("gray20", "firebrick1"),
+		      axes=FALSE, xlab="", ylab="", bty="n")
+		box(which = "plot", lty = "solid", lwd=10)
+		dev.off()
+		
+		# HALF MILE
+		halfraster <- raster(halfpixels)
+		png(file="Graphics/halfraster.png", width=2000, height=2000)
+		image(t(flip(halfraster,1)), col=c("gray20", "firebrick1"),
+		      axes=FALSE, xlab="", ylab="", bty="n")
+		box(which = "plot", lty = "solid", lwd=10)
+		dev.off()
+		
+		# WHOLE MILE
+		wholeraster <- raster(wholepixels)
+		png(file="Graphics/wholeraster.png", width=2000, height=2000)
+		image(t(flip(wholeraster,1)), col=c("gray20", "firebrick1"),
+		      axes=FALSE, xlab="", ylab="", bty="n")
+		box(which = "plot", lty = "solid", lwd=10)
+		dev.off()
+		
+		# image(t(flip(x, 1)))
+		# image(t(flip(x, 2)))
+		# plotRGB(t(flip(x, 1)))
+		# plotRGB(t(flip(x, 2)))
 		hotspots <- as.matrix(clump(crimeraster, directions=8, gaps=FALSE))
-
 		# make list that returns indices of each pixel in each hotspot
 		numhotspots <- max(hotspots, na.rm=TRUE)
 		hotspotlist <- vector("list", numhotspots)
@@ -122,12 +203,14 @@
 		source("RScript/DIST.R")
 		source("RScript/DISTppoly.R")
 		# each park is a row and each hotspot is a column
-		# distancematrix <- matrix(data=NA, nrow=length(unique(parkdat$number)), ncol=nrow(centers))
 		# loop through every park calculating distances to every hotspot
 		# DISTppoly should place a vector of distances in each row
-		distances <- matrix(data=NA, nrow=length(unique(parkdat$parknumber)), ncol=nrow(centers))
+		distances <- matrix(data=NA,
+				    nrow=length(unique(parkdat$parknumber)),
+				    ncol=nrow(centers))
 		for (i in 1:nrow(distances)) {
-			distances[i,] <- DISTppoly(pts=as.matrix(centers), poly=as.matrix(parkdat[parkdat$parknumber==i, 1:2]),
+			distances[i,] <- DISTppoly(pts=as.matrix(centers),
+						   poly=as.matrix(parkdat[parkdat$parknumber==i, 1:2]),
 						   method="Euclidean")
 		}
 
@@ -136,3 +219,26 @@
 		mymins$mins <- apply(distances, 1, min)
 		mymins <- mymins[order(mymins[,2], decreasing=FALSE),] # order parks from closest minimum distance to hotspot to furthest
 		parkdat <- merge(parkdat, mymins, by="parknumber")
+		parksort <- parkdat[order(parkdat[,9], decreasing=TRUE),]
+		parkmins <- unique(parksort[,c(1,8,9)])
+		parkmins$mins <- (parkmins$mins/5280)
+		
+		# PLOTTING GRAPHS OF PARKS---------------------------------------------------
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
